616f1c946743f8aab281549b56017c3ff8f7a78d
KevinHuebner <huebner-kevin@web.de>
2013-12-09 23:18:51 +0100

    first login adapted

--- src/main/java/de/cbos/admin/controller/AdminController.java
+++ src/main/java/de/cbos/admin/controller/AdminController.java
@@ -1,28 +1,21 @@
-package de.cbos.general.controller;
+package de.cbos.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/** annotated as controller to be recognized by DispatcherServlet **/
 @Controller
-public class HomeController {
+public class AdminController {
 
 	/**mapps incoming requests by URL and Method**/
-	@RequestMapping(value="/*",method=RequestMethod.GET)
+	@RequestMapping(value="/admin",method=RequestMethod.GET)
 	public ModelAndView home() {
 		/**ModelAndView object: returned name describes which jsp should be rendered,
 		 * 						data can be saved
 		 */
 		ModelAndView modelAndView = new ModelAndView("home");
-		modelAndView.addObject("message", "Welcome to CboS");
-		return modelAndView;
-	}
-	
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	public ModelAndView login() {
-		ModelAndView modelAndView = new ModelAndView("login");
+		modelAndView.addObject("message", "Logged in as admin");
 		return modelAndView;
 	}
 }
