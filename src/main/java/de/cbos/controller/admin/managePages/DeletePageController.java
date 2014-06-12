e912cae52a3f1d7179cde5bf3d69b743a82d1a87
KevinHuebner <huebner-kevin@web.de>
2014-06-12 18:01:54 +0200

    Controller for deleting pages added

---
+++ src/main/java/de/cbos/controller/admin/managePages/DeletePageController.java
@@ -0,0 +1,39 @@
+package de.cbos.controller.admin.managePages;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import de.cbos.controller.admin.AdminController;
+import de.cbos.service.page.PageService;
+
+@Controller
+public class DeletePageController {
+	
+	@Autowired
+	private PageService pageService;
+	
+	@Autowired
+	private AdminController adminController;
+	
+	@RequestMapping(value="/admin/delete/${page.pageName}", method=RequestMethod.GET)
+	public ModelAndView deletePageConfirmation(@PathVariable String pageName) {
+		ModelAndView modelAndView = new ModelAndView("deletePage");
+		modelAndView.addObject(pageService.getPage(pageName));
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/admin", method=RequestMethod.DELETE)
+	public ModelAndView deletePage(HttpServletRequest request) {
+		String pageName = request.getParameter("pageToDelete");
+		pageService.deletePage(pageService.getPage(pageName));
+		ModelAndView modelAndView = adminController.adminPage();
+		modelAndView.addObject("message", "Page was successfully deleted");
+		return modelAndView;
+	}
+}
